c2cfd18e37e31fc67fe76dbd1fc98677d9f4f1df
koti <kotireddy086@gmail.com>
2026-02-25 09:11:13 +0530

    added new script in a new file

---
+++ src/test/java/GitPractice.java
@@ -0,0 +1,11 @@
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+
+		System.out.println("change 1");
+		System.out.println("change 2");
+		System.out.println("change 3");
+	}
+
+}
